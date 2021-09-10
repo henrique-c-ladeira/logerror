48fe72d346f028eae64d6606d7362320db48ee8a
Henrique <henrique.c.ladeira@gmail.com>
2021-09-10 17:29:03 -0300

    ✨ feat: add API endpoint to create log

--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from logs.models import ErrorLog
+from .helpers import internalError, badRequest, getRequestBody, methodNotAllowed, ok
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ValidationError
 
 # Create your views here.
+
+
+@csrf_exempt
+def log(request):
+    if (request.method != 'POST'):
+        return methodNotAllowed()
+    try:
+        body = getRequestBody(request.body)
+        error_log = ErrorLog.create(**body)
+        error_log.save()
+        return ok()
+    except (ValidationError, TypeError) as error:
+        return badRequest(error)
+    except Exception as error:
+        return internalError(error)
