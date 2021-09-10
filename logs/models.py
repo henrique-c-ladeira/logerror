d83dd2320bb97fd6b984e965caeb40b2b39f73b5
Henrique <henrique.c.ladeira@gmail.com>
2021-09-10 17:27:41 -0300

    ✨ feat: define create method and default string

--- logs/models.py
+++ logs/models.py
@@ -7,3 +7,11 @@ class ErrorLog(models.Model):
     datetime = models.DateTimeField(default=timezone.now)
     stack = models.TextField()
     message = models.CharField(max_length=200)
+
+    @classmethod
+    def create(cls, app_name, stack, message):
+        error_log = cls(app_name=app_name, stack=stack, message=message)
+        return error_log
+
+    def __str__(self) -> str:
+        return f'{self.app_name.upper()} at {self.datetime.strftime("%Y-%m-%d%H:%M:%S")}'
