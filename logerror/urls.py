62a7a9880b881ad149a397806a5d8342b477e2af
Henrique <henrique.c.ladeira@gmail.com>
2021-09-10 17:28:11 -0300

    ✨ feat: change url structure and customize admin

--- logerror/urls.py
+++ logerror/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('logs.urls')),
-    path('admin/', admin.site.urls)
+    path('log/', include('logs.urls')),
+    path('', admin.site.urls)
 ]
--- logs/admin.py
+++ logs/admin.py
@@ -3,3 +3,7 @@ from .models import ErrorLog
 
 # Register your models here.
 admin.site.register(ErrorLog)
+admin.site.site_header = "Error Logs"
+admin.site.site_title = "Error Logs"
+admin.site.index_title = "Error Logs"
+admin.site.site_url = None
