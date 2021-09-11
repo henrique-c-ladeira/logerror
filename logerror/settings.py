735a757ee34d6ef0c349440db832391dd7ffe699
Henrique <henrique.c.ladeira@gmail.com>
2021-09-11 19:29:31 -0300

    ⚙️ chore: set debug and secret_key env variables

--- logerror/settings.py
+++ logerror/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7cq73jp1@b&9o4!=q-psu7_%v-7zgm*2za96a9pb+g0#j5nm#d'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
