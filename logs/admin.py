bce61780937e4bffb5156c0817c8d79e7f1bf6b0
Henrique <henrique.c.ladeira@gmail.com>
2021-09-11 22:23:32 -0300

    ✨ feat: better table visualization in admin

--- logs/admin.py
+++ logs/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import ErrorLog
 
+
+@admin.register(ErrorLog)
+class ErrorLogAdmin(admin.ModelAdmin):
+    list_display = ('app_name', 'datetime', 'message')
+
+
 # Register your models here.
-admin.site.register(ErrorLog)
 admin.site.site_header = "Error Logs"
 admin.site.site_title = "Error Logs"
 admin.site.index_title = "Error Logs"
